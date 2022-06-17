246f30c9fea45f7e84bcef056a73aa18e22891a0
Ralph <leduchoabatr12@gmail.com>
2022-06-17 16:34:26 +0700

    Create User model

---
+++ exercise_2/resolver/app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email)
+#
+class User < ApplicationRecord
+end
---
+++ exercise_2/resolver/spec/factories/users.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email)
+#
+FactoryBot.define do
+  factory :user do
+    
+  end
+end
