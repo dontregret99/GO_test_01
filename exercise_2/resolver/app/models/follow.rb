2cfec2f9e9bda6aa0ac92296aa8157d3d82c6b98
Ralph <leduchoabatr12@gmail.com>
2022-06-17 17:46:12 +0700

    Add methods

--- exercise_2/resolver/app/models/follow.rb
+++ exercise_2/resolver/app/models/follow.rb
@@ -18,5 +18,16 @@
 #  fk_rails_...  (follower_id => users.id)
 #
 class Follow < ApplicationRecord
+
+  # Associations
   belongs_to :user, class_name: "User", foreign_key: "user_id"
+
+  # callback
+  after_commit :update_followers_count
+
+  private
+  
+  def update_followers_count
+    self.user.update(followers_count: self.user.follows.count)
+  end
 end
--- exercise_2/resolver/app/models/user.rb
+++ exercise_2/resolver/app/models/user.rb
@@ -2,16 +2,35 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string           not null
-#  username   :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  followers_count :integer          default(0)
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 # Indexes
 #
 #  index_users_on_email  (email)
 #
 class User < ApplicationRecord
-  has_many :follows, class_name: "Follow"
+
+  # Associations
+  has_many :follows, class_name: "Follow", dependent: :destroy
+
+  def self.top_popular_users
+    User.all.order(followers_count: :desc)
+  end
+
+  def followers
+    self.follows
+  end
+
+  def followings
+    Follow.all.where(follower_id: self.id)
+  end
+
+  def new_followers_in_current_month
+    self.follower.where("created_at >= ?", Date.current.beginning_of_month)
+  end
 end
